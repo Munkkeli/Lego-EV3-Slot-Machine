f43bd29113bb4c120f69bbed801a92d98d527f85
Mikko Romo <mikko1.romo@gmail.com>
2017-09-29 13:09:41 +0300

    aaaaaa

--- src/Laskuri.java
+++ src/Laskuri.java
@@ -1,4 +1,53 @@
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.Color;
+import lejos.utility.Delay;
 
 public class Laskuri {
+
+	private int raha = 0;
+	
+	public Laskuri() {
+		
+		this.raha = raha;
+		
+	}
+
+	public void laskuri() {
+		
+	EV3ColorSensor cs = new EV3ColorSensor(SensorPort.S3);
+		
+	while(!Button.ESCAPE.isDown())
+	{
+
+		switch (cs.getColorID()) {
+
+		case Color.BLACK:
+			LCD.drawString("Saldo: " + raha * 0.20, 5, 6);
+			break;
+
+		default:
+			raha += 1;
+			LCD.drawString("Saldo: " + raha * 0.20, 5, 6);
+			Delay.msDelay(1000);
+			break;
+		}
+		LCD.refresh();
+		Delay.msDelay(200);
+	}
+	cs.close();
+	}
+	
+	
+
+	public int getRaha() {
+		return this.raha;
+	}
+
+	public void vahennaRaha(int i) {
+		this.raha -= i;
+	}
 
 }
