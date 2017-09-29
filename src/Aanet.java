58d1237142fcce60e94906261833570db90c782e
neeaoo <nneeeao@hotmail.com>
2017-09-29 13:00:07 +0300

    added sounds

---
+++ src/Aanet.java
@@ -0,0 +1,38 @@
+import lejos.hardware.Sound;
+	public class Aanet{ 
+	  
+	//paras mihin mä pystyn
+	
+		public static void PlayWin() {
+		 Sound.beepSequence();
+		 Sound.beepSequence();
+		 
+	 }
+	 
+	 public static void PlayWinwin() {
+		 Sound.beepSequenceUp();
+		 Sound.beepSequenceUp();
+		 
+	 }
+	 
+	  public static void PlayBigWin() throws InterruptedException {
+		  Sound.beepSequenceUp();
+		  Sound.playTone(287,200); 
+	      Thread.sleep(150);         
+
+	      Sound.playTone(392,200);  
+	      Thread.sleep(150);         
+
+	      Sound.playTone(440,200);  
+	      Thread.sleep(150);        
+		  Sound.beepSequenceUp();
+		  Sound.beepSequenceUp();
+	  }
+	  
+	  
+	  public static void PlayLoose() {
+		  Sound.buzz();
+		  Sound.buzz();
+	  }
+	   
+}
