18a1ad934cd71fe1b9e536fd104d8f60db975c69
Mikko Romo <mikko1.romo@gmail.com>
2017-09-29 13:08:47 +0300

    korjausta

--- src/Panos.java
+++ src/Panos.java
@@ -1,4 +1,24 @@
 
-public class Panos {
 
+	import lejos.hardware.lcd.LCD;
+
+	public class Panos extends Laskuri{
+		
+		private double panos = 0.20;
+
+		public void raiseBet() {
+			
+			if (getRaha() > 0 && panos >= 0.60) {
+				this.panos = 0.20;
+			} else if (getRaha() > 0){
+				this.panos += 0.20;
+			}
+			LCD.drawString("Panos: " + panos, 0, 7);
+		}
+		
+
+		public double getBet() {
+			return panos;
+		}
 }
+
